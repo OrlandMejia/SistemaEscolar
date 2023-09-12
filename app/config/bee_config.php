3a3f5e1f1ad97cee45a2da6d3b94ca96015bb8e4
OrlandMejia <124545378+OrlandMejia@users.noreply.github.com>
2023-09-12 00:05:25 -0600

    Update bee_config.php

--- app/config/bee_config.php
+++ app/config/bee_config.php
@@ -7,8 +7,8 @@
  * sean modificadas en el proceso por accidente así como el basepath y otras constantes que requieran
  * configuración especial en producción
  */
-define('IS_LOCAL'     , in_array($_SERVER['REMOTE_ADDR'], ['localhost', '::1']));
-define('BASEPATH'     , IS_LOCAL ? '/Sistema/SistemaEscolar/' : '/SistemaEscolar/'); // Debe ser cambiada a la ruta de tu proyecto en producción y desarrollo
+define('IS_LOCAL'     , in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']));
+define('BASEPATH'     , IS_LOCAL ? '/Sistema/SistemaEscolar/' : '____BASEPATH_______'); // Debe ser cambiada a la ruta de tu proyecto en producción y desarrollo
 define('IS_DEMO'      , false);
 
 // Set para conexión en producción o servidor real
